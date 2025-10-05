fc31dad053bf32642435593b06ca5720cdac29b9
Natalialuque <ncabluq878@g.educaand.es>
2025-10-05 21:42:26 +0200

    dwc

--- PrimerTRIMESTRE/Practicas/Ejercicios de JavaScript Básico I/main.js
+++ PrimerTRIMESTRE/Practicas/Ejercicios de JavaScript Básico I/main.js
@@ -1,23 +1,33 @@
 /**EJERCICIO 1 */
-//variables
-let nombre = document.getElementById("nombre");
-let apellidos = document.getElementById("apellido");
-let curso = document.getElementById("curso");
-let nota1 = document.getElementById("nota1");
-let nota2 = document.getElementById("nota2");
-let nota3 = document.getElementById("nota3");
-let añadir = document.getElementById("añadir");
-let alumnos = document.getElementById("alumnos");
 
-//ejercicio
-let listaAlumnos = [];
-listaNotas = [];
+    //variables 
+    let nombre = document.getElementById("nombre");
+    let apellidos = document.getElementById("ape");
+    let curso = document.getElementById("cur");
+    let nota1 = document.getElementById("nota1");
+    let nota2 = document.getElementById("nota2");
+    let nota3 = document.getElementById("nota3");
+    let parrafoAlum = document.getElementById("alum");
 
-añadir.onclick = function () {
-    listaNotas = [parseFloat(nota1.value), parseFloat(nota2.value), parseFloat(nota3.value)];
-    listaAlumnos = [nombre.value, apellidos.value, curso.value, listaNotas];
+    let añadir = document.getElementById("añadir");
+
+//ejecicio
+añadir.onclick = function() {
+
+    let notas = [nota1.value,nota2.value,nota3.value];
+    let alumnos = [nombre.value, apellidos.value, curso.value, notas];
+
+    for(let i=0;i<alumnos.length-1;i++){
+        parrafoAlum.innerHTML += alumnos[i] + " - ";
+    }
+
+     for(let i=0;i<alumnos.length-1;i++){
+        if(i==alumnos.length-2)
+            parrafoAlum.innerHTML += alumnos[3][i];
+        else
+            parrafoAlum.innerHTML += alumnos[3][i] + ", ";
+    }
+    parrafoAlum.innerHTML += "<br>"
 }
-
-alumnos.innerHTML = listaAlumnos;
 
 /**EJERCICIO 2 */
