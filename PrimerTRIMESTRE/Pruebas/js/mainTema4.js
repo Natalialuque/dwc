fc3929c61d624428cf2572be101f6d0acf4cd377
Natalialuque <ncabluq@g.educaand.es>
2025-11-04 13:00:39 +0100

    dwc

--- PrimerTRIMESTRE/Pruebas/js/mainTema4.js
+++ PrimerTRIMESTRE/Pruebas/js/mainTema4.js
@@ -1,3 +1,5 @@
+const { createElement } = require("react");
+
 let info = document.getElementById("info");
 let parrafos = document.getElementsByName("saludo");
 //let parrafos = document.getElementById("main").getElementsByTagName("");
@@ -39,35 +41,42 @@ const opciones = document.getElementsByName("opciones");
 // }
 
 ////////////////////////////////////////////////////
-let pie = document.createElement("footer");
-let pPie = document.createElement("p");
-let pPieText = document.createTextNode("Contacto: 654 654 654");
+// let pie = document.createElement("footer");
+// let pPie = document.createElement("p");
+// let pPieText = document.createTextNode("Contacto: 654 654 654");
 
-pPie.appendChild(pPieText);
+// pPie.appendChild(pPieText);
 
-pie.appendChild(pPie);
+// pie.appendChild(pPie);
 
-document.body.appendChild(pie);
+// document.body.appendChild(pie);
 
 
-document.getElementById("check").onclick = () => {
-    let op = document.getElementById("opcionLista").value;
-    let lista = null;
+// document.getElementById("check").onclick = () => {
+//     let op = document.getElementById("opcionLista").value;
+//     let lista = null;
     
-    if ((document.getElementsByTagName("ul").length > 0) && (op != "")) {
-        lista = document.getElementsByTagName("ul")[0];
+//     if ((document.getElementsByTagName("ul").length > 0) && (op != "")) {
+//         lista = document.getElementsByTagName("ul")[0];
 
-        let opElement = document.createElement("li");
-        let opElementText = document.createTextNode(op);
+//         let opElement = document.createElement("li");
+//         let opElementText = document.createTextNode(op);
 
-        opElement.appendChild(opElementText);
-        lista.appendChild(opElement);
-    }
+//         opElement.appendChild(opElementText);
+//         lista.appendChild(opElement);
+//     }
 
-    document.getElementById("opcionLista").value = "";
-}
+//     document.getElementById("opcionLista").value = "";
+// }
 
+let pie = document.createElement("footer");
 /*************************************************** */
 function createNode(tipoNodo,tipoTexto){
-    let nodo;
+    let nodo = createElement(tipoNodo);
+    let nodoText = createElement(tipoTexto);
+    nodo.appendChild( nodoText);
+
+    return nodo;
+    
+
 }
