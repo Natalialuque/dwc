bae0c629d396f6a92737bb76fe6a62e2451c9f2b
Natalialuque <ncabluq@g.educaand.es>
2025-11-11 12:11:43 +0100

    Dwccc

--- PrimerTRIMESTRE/Practicas/ejercicioEntregable2/js/main.js
+++ PrimerTRIMESTRE/Practicas/ejercicioEntregable2/js/main.js
@@ -9,111 +9,45 @@ const regexEmail = /^\S+@\S+\.\S+$/; // \S --> todo lo que no sea un espacio, un
 
 /********************************************************************************* */
 
-//ARREGLAR
-const usuarios = datosUsuarios.map((linea) => {
-  const nombreMatch = linea.match(/Nombre:\s*(.*?),/);
-  const emailMatch = linea.match(/Email:\s*(.*?),/);
-  const fechaMatch = linea.match(/FechaNac:\s*(.*)$/);
+document.getElementById("bDatosusuarios").onclick=function(){
 
-  return {
-    nombre: nombreMatch ? nombreMatch[1].trim() : null,
-    email: emailMatch ? emailMatch[1].trim() : null,
-    fechaNacimiento: fechaMatch ? fechaMatch[1].trim() : null
-  };
-});
+  for(let usuario = 0; usuario< datosUsuarios.length; usuario++){
+   
 
-console.log(usuarios);
+    //mostrar indices
+     let datosUsuario = datosUsuarios[usuario].split(",");
+    document.getElementById("indice"+usuario).innerHTML=usuario;
 
-/**Nombre completo sin epacios en los ectremos, y el nombre debe aparecer en mayusucula */
+    //obtener el nombre 
+    let nombre= datosUsuario[0].split(":")[1].trim().toUpperCase();
+    document.getElementById("nombre"+usuario).innerHTML=nombre;
 
-//Para eliminar espacios y poner en mayuscula
-function nombre(){
+    //obtener el email 
+    let email= datosUsuario[1].split(":")[1].trim();
+    document.getElementById("email"+usuario).innerHTML=email;
 
-}
-
-
-
-
- 
-//Para sacar la edad del usuario
-function edad(){
-      const nacimiento = new Date(/*datosUsuarios[""]=""*/);
-      const hoy = new Date(); //
-
-      let edadAnios = ahora.getFullYear() - nacimiento.getFullYear();
-
-     if (ahora < cumpleEsteAnio) {
-        edadAnios--; // Aún no ha cumplido años este año
-      }
-
-      return edadAnios;
-}
-
-
-
-//Para validar el email
- function validaEmail(email){
-    
-     if(email.match(regexEmail)){
-       return "si";
+    if(email.match(regexEmail)){
+          document.getElementById("valido"+usuario).innerHTML="valido ";
     }else{
-         return "no";     
+        document.getElementById("valido"+usuario).innerHTML="No valido ";
     }
 
- }
-
-//variables
-let botonDatos = document.getElementById("bDatosusuarios");
-
-let nombre0=document.getElementById("nombre0")
-let nombre1=document.getElementById("nombre0")
-let nombre2=document.getElementById("nombre0")
-let nombre3=document.getElementById("nombre0")
-
-let email0=document.getElementById("email0");
-let email1=document.getElementById("email1");
-let email2=document.getElementById("email2");
-let email3=document.getElementById("email3");
-
-let edad0=document.getElementById("edad0");
-let edad1=document.getElementById("edad1");
-let edad2=document.getElementById("edad2");
-let edad3=document.getElementById("edad3");
-
-let valido0 = document.getElementById("valido0");
-let valido1 = document.getElementById("valido1");
-let valido2 = document.getElementById("valido2");
-let valido3 = document.getElementById("valido3");
+    //obtener años 
+    let fechaAño = new Date(datosUsuario[2].split(":")[1].trim());
+    //alert(fechaAño.getFullYear());
+    
+    let annoNow = new Date().getFullYear();
+    
+   if(fechaAño)
+     document.getElementById("edad"+usuario).innerHTML=annoNow-fechaAño.getFullYear();
+   else   
+      document.getElementById("valido"+usuario).innerHTML="Fecha no admitida ";
 
 
-botonDatos.onclick=function(){
-
-    //nombre
-
-    //edad
-    edad0.innerHTML=edad();
-    edad1.innerHTML=edad();
-    edad2.innerHTML=edad();
-    edad3.innerHTML=edad();
-
-    //email
-        email0.innerHTML=datosUsuarios["Email"]=" ana.garcia@example.com";
-        email1.innerHTML=datosUsuarios["Email"]=" luisperez@dominio.net";
-        email2.innerHTML=datosUsuarios["Email"]=" marta.fdez-INVALID";
-        email3.innerHTML=datosUsuarios["Email"]="  pepe.lopez@example.es";
-
-
-    //validar
-         valido0.innerHTML=validaEmail(email0);
-         valido1.innerHTML=validaEmail(email1);
-         valido2.innerHTML=validaEmail(email1);
-         valido3.innerHTML=validaEmail(email2);
-         valido4.innerHTML=validaEmail(email3);
-
+    datosUsuario[0];
+  }
 
 }
-
-
 
 //ventana abre
 let ancho = (window.innerWidth-50);
